83e713ed03baa5fe99106a4f8031c06338df196a
kavitha Rayi <kunalrai955@gmail.com>
2019-08-04 13:42:30 +0530

    initial structure

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        int z = 30;
+
+        System.out.println("sum: " + c);
+
+        {
+            z = 50;
+
+        }
+    }
+
+
+}
+
